d53480b975ee4d9f5df588e0e574aa684ff74718
Lydia Young <lydsjoyy@gmail.com>
2024-02-03 15:57:30 +0000

    add comment model to blog

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+# From I think before I blog walkthrough and Youtube Video for image
 class Post(models.Model): 
     title = models.CharField(max_length=200, unique = True)
     slug = models.SlugField(max_length=200, unique = True)
@@ -13,6 +14,15 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True) 
+    excerpt = models.TextField(blank=True)
 
+# from I think before I blog walkthrough
+class Comment(Models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
 
