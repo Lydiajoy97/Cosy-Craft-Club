6c6f8c3df2e3676d81908eb62e4276e4a689df33
Lydia Young <lydsjoyy@gmail.com>
2024-02-03 16:13:37 +0000

    add created on meta and self title

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+class Meta:
+        ordering = ["-created_on"]
+def __str__(self):
+    return f"This artwork is titled {self.title}"
 
 # from I think before I blog walkthrough
 class Comment(models.Model):
@@ -25,3 +29,4 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
