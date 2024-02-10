b1f0b2ce33d183530c6c6e51b7d78826c3582125
Lydia Young <lydsjoyy@gmail.com>
2024-02-10 10:33:38 +0000

    install summernote and make changes to admin post

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+# Post admin list from the walkthrough
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'excerpt', 'created_on', 'status')
+    list_filter = ('status',)
+    search_fields = ['title']
+    summernote_fields = ('content',)
+    prepopulated_fields = {'slug': ('title',)}
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
