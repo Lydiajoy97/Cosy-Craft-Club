978496ba1f6ecde1250a6bdf4d51c86cfc2c3ee0
Lydia Young <lydsjoyy@gmail.com>
2024-02-11 18:07:33 +0000

    edit model

--- cosycraftclub/settings.py
+++ cosycraftclub/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-!wl390%ywzju#2l8%#@o(yi&w=vfma7ex=q3pg3f#l%^_nle+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 
 ALLOWED_HOSTS = ['8000-lydiajoy97-cosycraftclu-d0ekvem0x34.ws-eu108.gitpod.io', '.herokuapp.com',
--- gallery/models.py
+++ gallery/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 # From I think before I blog walkthrough and Youtube Video for image
 class Uploads(models.Model): 
+    title = models.CharField(max_length=200, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
     artist = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="art_uploads")
@@ -15,7 +16,7 @@ class Uploads(models.Model):
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
-        return f"This artwork is titled {self.title} | uploaded by {self.author}"
+        return f"This artwork is titled {self.title} | uploaded by {self.artist}"
 
 # from I think before I blog walkthrough
 class Comment(models.Model):
