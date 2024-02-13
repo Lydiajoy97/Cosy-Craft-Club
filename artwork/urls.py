cdf9ce70e041a6fdb21e3f0a27c40d194515dfbe
Lydia Young <lydsjoyy@gmail.com>
2024-02-13 21:03:55 +0000

    adduploads_detail veiw

--- artwork/urls.py
+++ artwork/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
      path('', views.UploadsList.as_view(), name='home'),
+     path('<slug:slug>/', views.Uploads_detail, name='uploads_detail'),
 ]
--- artwork/views.py
+++ artwork/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from django.views import generic
 from .models import Uploads
 
 # Create your views here.
 class UploadsList(generic.TemplateView):
-  #  queryset = Uploads.objects.all()
     template_name = "index.html"
-  #  paginate_by = 3
+
+def Uploads_detail(request, slug):
+    """
+    Display an individual :model:`Artwork.Uploads`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`Artwork.Uploads`.
+
+    **Template:**
+
+    :template:`artwork/Uploads_detail.html`
+    """
+
+    queryset = Uploads.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "artwork/Uploads_detail.html",
+        {"post": post},
+    )
