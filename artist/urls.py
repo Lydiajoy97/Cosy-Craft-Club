64029676cc3e5f1eabb3925da10ddc292e5f1390
Lydia Young <lydsjoyy@gmail.com>
2024-02-17 23:03:55 +0000

    fix

--- artist/urls.py
+++ artist/urls.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('artwork.urls')),
-    path("accounts/", include("allauth.urls")),
-    path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-]
+    path('', include('artwork.urls')),]
